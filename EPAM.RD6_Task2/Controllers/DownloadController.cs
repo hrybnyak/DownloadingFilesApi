2ff88e2d0b5be49182010bb7e3c619d1d459bff5
hrybnyak <hrybnyak.y@gmail.com>
2019-12-09 16:26:18 +0200

    Initial commit

---
+++ EPAM.RD6_Task2/Controllers/DownloadController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NLog;
+namespace EPAM.RD6_Task2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DownloadController : ControllerBase
+    {
+        private static readonly Logger _logger = LogManager.GetCurrentClassLogger();
+        private readonly DownloadingFilesService _service = new DownloadingFilesService();
+        [HttpGet]
+        [Route("sync")]
+        public ActionResult DownloadFilesSync()
+        {
+            try
+            {
+                TimeSpan time = _service.DownloadFiles();
+                _logger.Info("User called synchronous controller and it finished in " + time.TotalMilliseconds + " ms.");
+                return NoContent();
+            }
+            catch
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpGet]
+        [Route("async")]
+        public async Task<ActionResult> DownloadFilesAsync()
+        {
+            try
+            {
+                TimeSpan time = await _service.DownloadFilesAsync();
+                _logger.Info("User called asynchronous controller and it finished in " + time.TotalMilliseconds + " ms.");
+                return NoContent();
+            }
+            catch
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpGet]
+        [Route("parallel")]
+        public ActionResult DownloadFilesParallel()
+        {
+            try
+            {
+                TimeSpan time = _service.DownloadFilesParallel();
+                _logger.Info("User called parallel controller and it finished in " + time.TotalMilliseconds + " ms.");
+                return NoContent();
+            }
+            catch
+            {
+                return BadRequest();
+            }
+        }
+    }
+}
---
+++ EPAM.RD6_Task2/DownloadingFilesService.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EPAM.RD6_Task2
+{
+    public class DownloadingFilesService
+    {
+        private Stopwatch _stopWatch;
+        private static string Url => "http://download.microsoft.com/download/c/6/9/c6938ba7-02cf-49d0-b7bf-88fd88a04024/";
+        private static string EndOfUrl => "18msdn_emag.pdf";
+        private Stopwatch Stopwatch
+        {
+            get
+            {
+                if(_stopWatch == null)
+                {
+                    _stopWatch = new Stopwatch();
+                }
+                return _stopWatch;
+            }
+        }
+        public void CreatePathes(int i, out string adress, out string filePath)
+        {
+            adress = "";
+            string journalNumber;
+            if (i < 10)
+            {
+                journalNumber = "0" + i;
+            }
+            else
+            {
+                journalNumber = i.ToString();
+            }
+            StringBuilder sb = new StringBuilder();
+            sb.Append(Url);
+            sb.Append(journalNumber);
+            sb.Append(EndOfUrl);
+            adress = sb.ToString();
+            sb.Clear();
+            sb.Append(Environment.CurrentDirectory);
+            sb.Append(@"\files\");
+            sb.Append(i);
+            sb.Append(".pdf");
+            filePath = sb.ToString();
+        }
+
+        public TimeSpan DownloadFiles()
+        {
+            Stopwatch.Restart();
+            for (int i = 1; i < 13; i++)
+            {
+                using (WebClient wb = new WebClient())
+                {
+                    CreatePathes(i, out string adress, out string path);
+                    wb.DownloadFile(adress, path);
+                }
+            }
+          
+            Stopwatch.Stop();
+            return Stopwatch.Elapsed;
+        }
+
+        public async Task<TimeSpan> DownloadFilesAsync()
+        {
+            Stopwatch.Restart();
+            for (int i = 1; i < 13; i++)
+            {
+                using (WebClient _client = new WebClient())
+                {
+                    CreatePathes(i, out string adress, out string path);
+                    await _client.DownloadFileTaskAsync(adress, path);
+                }
+            }
+            Stopwatch.Stop();
+            return Stopwatch.Elapsed;
+        }
+
+        public TimeSpan DownloadFilesParallel()
+        {
+            Stopwatch.Restart();
+            
+                Parallel.For(1, 13, i =>
+                {
+                    using (WebClient _client = new WebClient())
+                    {
+                        CreatePathes(i, out string adress, out string path);
+                        _client.DownloadFile(adress, path);
+                    }
+                });
+            
+            Stopwatch.Stop();
+            return Stopwatch.Elapsed;
+        }
+    }
+}
